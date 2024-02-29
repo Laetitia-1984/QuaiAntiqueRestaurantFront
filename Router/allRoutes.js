b13df45298f2ffe19aefa2d1a9d77ba0c81ccf66
Laetitia Bouslah <laetitia.bouslah@free.fr>
2024-02-29 14:06:46 +0100

    Ajout page Reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement du mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+
 
 ];
 
